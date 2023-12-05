bf7eba6f1abc0706798d91eccd330b7bcba75815
cherishjh <lemony3741@gmail.com>
2023-12-05 11:29:08 +0900

    checking

--- src/C00HelloWorld.java
+++ src/C00HelloWorld.java
@@ -1,5 +1,11 @@
 public class C00HelloWorld {
     public static void main(String[] args) {
-
+//    문자열을 출력할 때 쌍따옴표로 문자열을 감싼다
+        System.out.println("hello world");
+        int a =20;
+        System.out.println(a);
+        String myString = "hello world";
+//        문자열과 숫자를 합하면
+        System.out.println(myString);
     }
 }
