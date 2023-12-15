f99b74c0fa96ef0e9699f258464a86806e81d2b9
cherishjh <lemony3741@gmail.com>
2023-12-15 17:46:07 +0900

    add,DEC

--- src/C12ClassLecture/AuthorPostService_Lec2.java
+++ src/C12ClassLecture/AuthorPostService_Lec2.java
@@ -1,18 +1,14 @@
 package C12ClassLecture;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-
-public class AuthorPostService {
-    public static void main(String[] args) {
-            List<Author> authors = new ArrayList<>();
-            List<Post> posts = new ArrayList<>();
+public class AuthorPostService_Lec2 {
+public static void main(String[] args) {
+    List<Author> authors = new ArrayList<>();
+    List<Post> posts = new ArrayList<>();
             while(true){
+                System.out.println("1번:회원가입, 2번:게시글작성, 3번:회원목록조회, 4번:회원상세조회, 5번:게시글상세조회 ");
                 Scanner sc = new Scanner(System.in);
                 int number = Integer.parseInt(sc.nextLine());
-                System.out.println("1번:회원가입, 2번:게시글작성, 3번:회원목록조회, 4번:회원상세조회, 5번:게시글상세조회 ");
                 switch (number){
                     case 1:
                         System.out.println("회원가입하실 이름을 입력해주세요");
@@ -23,14 +19,27 @@ public class AuthorPostService {
                         authors.add(author1);
                         break;
                     case 2:
+                        System.out.println("작성자 email를 입력해주세요");
+                        String author_email = sc.nextLine();
+                        Author temp_author = null;
+                        for(Author a : authors){
+                            if(a.getEmail().equals(author_email)){
+                                temp_author = a;
+                                break;
+                            }
+                        }
+                        if (temp_author==null){
+                            System.out.println("없는 사용자입니다.");
+                            continue;
+                        }
                         System.out.println("게시글명을 입력해주세요");
                         String title = sc.nextLine();
                         System.out.println("게시글 내용을 입력해주세요");
                         String contents = sc.nextLine();
-                        System.out.println("작성자 id를 입력해주세요");
-                        Long id = Long.parseLong(sc.nextLine());
-                        Post post1 = new Post(title, contents, id);
+                        Post post1 = new Post(title, contents, temp_author);
+//                    작성자의 객체의 post리스트에 포스트를 add
                         posts.add(post1);
+                        temp_author.addPost(post1);
                         break;
 //                    회원목록조회
                     case 3:
@@ -41,21 +50,15 @@ public class AuthorPostService {
 //                    회원상세조회 : 이름, email, 작성글수
                     case 4:
                         System.out.println("author email를 입력해주세요");
-                        String author_email = sc.nextLine();
-                        Author temp_author = null;
+                        String author_email2 = sc.nextLine();
+                        Author temp_author2 = null;
                         for(Author a : authors){
-                            if(a.getId().equals(author_email)){
-                                temp_author = a;
+                            if(a.getEmail().equals(author_email2)){
+                                temp_author2 = a;
                                 break;
                             }
                         }
-                        int count = 0;
-                        for(Post p : posts){
-                            if(p.getAuthor_id() == temp_author.getId()){
-                                count++;
-                            }
-                        }
-                        System.out.println(temp_author.getName() + " " + temp_author.getEmail() + " " + count);
+                        System.out.println(temp_author2.getName() + " 이고 " + "작성글 수는 " + temp_author2.getPosts().size());
 
                         break;
 //                    게시글상세조회
@@ -69,13 +72,7 @@ public class AuthorPostService {
                                 break;
                             }
                         }
-                        Author temp_post_author = null;
-                        for(Author a : authors){
-                            if(a.getId() == temp_post.getAuthor_id()){
-                                temp_post_author = a;
-                            }
-                        }
-                        System.out.println(temp_post.getTitle() + " " + temp_post_author.getEmail());
+                        System.out.println(temp_post.getTitle() + " " + temp_post.getAuthor().getEmail());
                         break;
                 }
 
@@ -88,12 +85,18 @@ public class AuthorPostService {
         private Long id;
         private String name;
         private String email;
+        //    포스트 리스트 변수
+        private List<Post> posts;
         static Long static_id = 0L;
         Author(String name, String email){
             static_id += 1;
             this.id = static_id;
             this.name = name;
             this.email = email;
+            this.posts = new ArrayList<>();
+        }
+        void addPost(Post post){
+            this.posts.add(post);
         }
 
         public Long getId() {
@@ -108,19 +111,23 @@ public class AuthorPostService {
             return email;
         }
 
+        public List<Post> getPosts() {
+            return posts;
+        }
     }
     class Post{
         private Long id;
         private String title;
         private String contents;
-        private Long author_id;
+        private Author author;
         static Long static_id = 0L;
-        Post(String title, String contents, Long author_id){
+        Post(String title, String contents, Author author){
             static_id += 1;
+            this.author = author;
+//        this.author.addPost(this);
             this.id = static_id;
             this.title = title;
             this.contents = contents;
-            this.author_id = author_id;
         }
 
         public Long getId() {
@@ -135,7 +142,11 @@ public class AuthorPostService {
             return contents;
         }
 
-        public Long getAuthor_id() {
-            return author_id;
+        public String getPostInfo(){
+            return this.author.getEmail() + this.title + this.contents;
+        }
+        public Author getAuthor() {
+            return this.author;
         }
     }
+
